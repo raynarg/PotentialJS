5bff78a2c8c09a01495e94a457184b5f16d24019
raynarg <santiagorayn24@gmail.com>
2025-08-16 11:00:33 -0300

    02-Primitivos.js

---
+++ 01-tipos/02-Primitivos.js
@@ -0,0 +1,6 @@
+let numero = 1;
+let texto = "Hola Mundo";
+let verdadero = true;
+let falso = false;
+//va a ser del tipo de  dato undefined, aquel que todas las variables las cuales no se les haya asignado un valor al declararlas
+let noDefinido;
